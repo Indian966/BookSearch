c117b43627c52f8dcf0ab619f88f5adccf0a1b38
Indian966 <rlaalsrb4175@gmail.com>
2019-04-13 17:04:20 +0900

    4/13 수정
    메니페스트, 메인 액티비티 추가

---
+++ app/src/main/java/kr/ac/jbnu/se/stkim/activities/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package kr.ac.jbnu.se.stkim.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import kr.ac.jbnu.se.stkim.R;
+
+public class MainMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
