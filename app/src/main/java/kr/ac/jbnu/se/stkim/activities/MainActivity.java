c5fa2e711421ded7bd1778ae0e723fb108e7953a
Indian966 <rlaalsrb4175@gmail.com>
2019-04-18 01:07:33 +0900

    음악+메인메뉴

--- app/src/main/java/kr/ac/jbnu/se/stkim/activities/MainActivity.java
+++ app/src/main/java/kr/ac/jbnu/se/stkim/activities/MainActivity.java
@@ -1,21 +1,31 @@
 package kr.ac.jbnu.se.stkim.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+
 
 import kr.ac.jbnu.se.stkim.R;
 
-public class MainMenuActivity extends Activity {
+public class MainActivity extends Activity {
     private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mp = MediaPlayer.create(getBaseContext(), R.raw.Mattia_Vlad_Morleo___Sinless);
-        mp.setLooping(true);
-        mp.start();
 
+        mp = MediaPlayer.create(this, R.raw.bgm1);
+        mp.setLooping(true);
+        mp.start();//음악재생
     }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, BookListActivity.class);
+        startActivity(intent);
+    }
+
 }
